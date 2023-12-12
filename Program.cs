ffea4e912bd7ea84bda4eeb647d5069f71d72cc9
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:05:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä lause: ");
+        string lause = Console.ReadLine();
+
+        // Erotellaan lause sanaparsiksi
+        string[] sanat = lause.Split(' ');
+
+        string pisinSana = "";
+
+        foreach (string sana in sanat)
+        {
+            // Poistetaan mahdolliset erikoismerkit sanoista (esim. pilkut, pisteet)
+            string puhdistettuSana = new string(sana.Where(char.IsLetter).ToArray());
+
+            if (puhdistettuSana.Length > pisinSana.Length)
+            {
+                pisinSana = puhdistettuSana;
+            }
+        }
+
+        Console.WriteLine($"Pisimmän sanan pituus on {pisinSana.Length} merkkiä:");
+        Console.WriteLine(pisinSana);
+    }
+}
